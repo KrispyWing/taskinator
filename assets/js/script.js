494583c897f161200ce25bf06d7cc0ef913a777b
Kris Pennimpede <kpennimpede@hotmail.com>
2021-01-02 12:35:11 -0500

    update add task function to display form data

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,24 @@ var tasksToDoEl = document.querySelector("#tasks-to-do");
 var createTaskHandler = function(event) {
     
     event.preventDefault();
-    
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    //Create List Item
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new Task.";
+
+    //Create Div to hold task info and add to list item
+    var taskInfoEl = document.createElement("div");
+    
+    //Give it a class name
+    taskInfoEl.className = "task-info";
+    
+    //Add HTML content to div
+    taskInfoEl.innerHTML = "<h3 class='task-name'>" + taskNameInput + "</h3><span class='task-type'>" + taskTypeInput + "</span>";
+    listItemEl.appendChild(taskInfoEl);
+
+    //Add entire list item to the list
     tasksToDoEl.appendChild(listItemEl);
 };
 
